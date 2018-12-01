a8a9536fab7a04c07b1b89fd04a46ec04558d9b3
Daniel <studis22@gmail.com>
2018-12-01 18:29:04 +0300

    2+

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-})
+$(function() {
+     /* Top Menu class Toggle */
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 90) {
+            $('.navbar').addClass('shadow-sm');
+        }
+        else {
+            $('.navbar').removeClass('shadow-sm');
+        }
+    });
+
+});
